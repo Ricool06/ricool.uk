5c071c01fa42d44376f75d8ec5ac9f4fbdd8973b
Ricool06 <ricoolohsix@hotmail.com>
2019-02-08 12:52:11 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-#My personal site, [ricool.uk](http://ricool.uk)
-Created with [Bulma](http://bulma.io), customised using [Sass](http://sass-lang.com).
--- api/getpage.php
+++
@@ -1,7 +0,0 @@
-<?php
-    /*
-        Gets page to display in site content.
-    */
-    //$db = new SQLite3($_SERVER['DOCUMENT_ROOT'] . '/../ricool.uk.db');
-
- ?>
